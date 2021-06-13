3bb29302857900ddb91f2f878ac5194c29eb7bf3
Jan <56030577+janek1842@users.noreply.github.com>
2021-06-13 12:25:30 +0200

    Add files via upload

---
+++ Blogproject/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'db_host'     => 'localhost',
+    'db_name'     => 's047',
+    'db_user'     => 's047',
+    'db_password' => 'k5nfy8jc'
+
+];
+ $config['recaptcha_public'] = '6LcrreYUAAAAAGeERM9Etdv8wlh81RCSK0LXuwfX';
+ $config['recaptcha_private'] = '6LcrreYUAAAAALTdoToeer_H4NZ1ECK4U76g0huL';
